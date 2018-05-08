047887d11ad2c99f0bcba8420002a38363cf6784
Thomas Qvarnström <tqvarnst@redhat.com>
2018-05-08 13:10:35 -0700

    Added Tracing to the Vert.x demo

--- gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/GatewayVerticle.java
+++ gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/GatewayVerticle.java
@@ -7,6 +7,7 @@ import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.ext.web.Router;
 import io.vertx.rxjava.ext.web.RoutingContext;
+import io.vertx.rxjava.ext.web.client.HttpRequest;
 import io.vertx.rxjava.ext.web.client.WebClient;
 import io.vertx.rxjava.ext.web.codec.BodyCodec;
 import io.vertx.rxjava.ext.web.handler.CorsHandler;
@@ -17,17 +18,37 @@ import org.slf4j.LoggerFactory;
 import rx.Observable;
 import rx.Single;
 
+import static com.redhat.cloudnative.gateway.TracingInterceptor.propagate;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class GatewayVerticle extends AbstractVerticle {
     private static final Logger LOG = LoggerFactory.getLogger(GatewayVerticle.class);
 
+    static final List<String> TRACING_HEADERS = new ArrayList<String>() {{
+        add("x-request-id");
+        add("x-b3-traceid");
+        add("x-b3-spanid");
+        add("x-b3-parentspanid");
+        add("x-b3-sampled");
+        add("x-b3-flags");
+        add("x-ot-span-context");
+    }};
+
+    private Map<String, String> tracingHeaders;
+
+
     private WebClient catalog;
     private WebClient inventory;
+
 
     @Override
     public void start() {
         Router router = Router.router(vertx);
         router.route().handler(CorsHandler.create("*").allowedMethod(HttpMethod.GET));
         router.get("/health").handler(ctx -> ctx.response().end(new JsonObject().put("status", "UP").toString()));
+        router.route().handler(TracingInterceptor.create());
         router.get("/api/products").handler(this::products);
 
         ServiceDiscovery.create(vertx, discovery -> {
@@ -35,15 +56,15 @@ public class GatewayVerticle extends AbstractVerticle {
             Single<WebClient> catalogDiscoveryRequest = HttpEndpoint.rxGetWebClient(discovery,
                     rec -> rec.getName().equals("catalog"))
                     .onErrorReturn(t -> WebClient.create(vertx, new WebClientOptions()
-                            .setDefaultHost(System.getProperty("catalog.api.host", "catalog-coolstore-vertx.192.168.64.13.nip.io"))
-                            .setDefaultPort(Integer.getInteger("catalog.api.port", 80))));
+                            .setDefaultHost(System.getProperty("catalog.api.host", "catalog"))
+                            .setDefaultPort(Integer.getInteger("catalog.api.port", 8080))));
 
             // Inventory lookup
             Single<WebClient> inventoryDiscoveryRequest = HttpEndpoint.rxGetWebClient(discovery,
                     rec -> rec.getName().equals("inventory"))
                     .onErrorReturn(t -> WebClient.create(vertx, new WebClientOptions()
-                            .setDefaultHost(System.getProperty("inventory.api.host", "inventory-coolstore-vertx.192.168.64.13.nip.io"))
-                            .setDefaultPort(Integer.getInteger("inventory.api.port", 80))));
+                            .setDefaultHost(System.getProperty("inventory.api.host", "inventory"))
+                            .setDefaultPort(Integer.getInteger("inventory.api.port", 8080))));
 
             // Zip all 3 requests
             Single.zip(catalogDiscoveryRequest, inventoryDiscoveryRequest, (c, i) -> {
@@ -59,7 +80,7 @@ public class GatewayVerticle extends AbstractVerticle {
 
     private void products(RoutingContext rc) {
         // Retrieve catalog
-        catalog.get("/api/catalog.json").as(BodyCodec.jsonArray()).rxSend()
+        propagate(catalog,rc).get("/api/products").as(BodyCodec.jsonArray()).rxSend()
                 .map(resp -> {
                     if (resp.statusCode() != 200) {
                         new RuntimeException("Invalid response from the catalog: " + resp.statusCode());
@@ -71,17 +92,19 @@ public class GatewayVerticle extends AbstractVerticle {
                         Observable.from(products)
                                 .cast(JsonObject.class)
                                 .flatMapSingle(product ->
-                                        inventory.get("/api/inventory.json").as(BodyCodec.jsonObject())
-                                                .rxSend()
-                                                .map(resp -> {
-                                                    if (resp.statusCode() != 200) {
-                                                        LOG.warn("Inventory error for {}: status code {}",
-                                                                product.getString("itemId"), resp.statusCode());
-                                                        return product.copy();
-                                                    }
-                                                    return product.copy().put("availability",
-                                                            new JsonObject().put("quantity", resp.body().getInteger("quantity")));
-                                                }))
+                                {
+                                     propagate(inventory,rc).get("/api/inventory/" + product.getString("itemId")).as(BodyCodec.jsonObject())
+                                            .rxSend()
+                                            .map(resp -> {
+                                                if (resp.statusCode() != 200) {
+                                                    LOG.warn("Inventory error for {}: status code {}",
+                                                            product.getString("itemId"), resp.statusCode());
+                                                    return product.copy();
+                                                }
+                                                return product.copy().put("availability",
+                                                        new JsonObject().put("quantity", resp.body().getInteger("quantity")));
+                                            });
+                                })
                                 .toList().toSingle()
                 )
                 .subscribe(
---
+++ gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/TracingInterceptor.java
@@ -0,0 +1,56 @@
+package com.redhat.cloudnative.gateway;
+
+import io.vertx.core.Handler;
+import io.vertx.ext.web.client.impl.WebClientInternal;
+import io.vertx.rxjava.ext.web.RoutingContext;
+import io.vertx.rxjava.ext.web.client.WebClient;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Propagate OpenTracing HTTP headers.
+ */
+public class TracingInterceptor {
+
+    private static final List<String> FORWARDED_HEADER_NAMES = Arrays.asList(
+            "x-request-id",
+            "x-b3-traceid",
+            "x-b3-spanid",
+            "x-b3-parentspanid",
+            "x-b3-sampled",
+            "x-b3-flags",
+            "x-ot-span-context",
+            "user-agent"
+    );
+
+    public static final String X_TRACING_HEADERS = "X-Tracing-Headers";
+
+    public static Handler<RoutingContext> create() {
+        return rc -> {
+            Set<String> names = rc.request().headers().names();
+            Map<String, List<String>> headers = names.stream()
+                    .map(String::toLowerCase)
+                    .filter(FORWARDED_HEADER_NAMES::contains)
+                    .collect(Collectors.toMap(
+                            Function.identity(),
+                            h -> Collections.singletonList(rc.request().getHeader(h))
+                    ));
+            rc.put(X_TRACING_HEADERS, headers);
+            rc.next();
+        };
+    }
+
+    public static WebClient propagate(WebClient client, RoutingContext rc) {
+        WebClientInternal delegate = (WebClientInternal) client.getDelegate();
+        delegate.addInterceptor(ctx -> {
+            Map<String, List<String>> headers = rc.get(X_TRACING_HEADERS);
+            if (headers != null) {
+                headers.forEach((s, l) -> l.forEach(v -> ctx.request().putHeader(s, v)));
+            }
+            ctx.next();
+        });
+        return client;
+    }
+}
